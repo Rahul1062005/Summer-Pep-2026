19913736d3a0e8a8195e0151a9df76bf96acea22
Rahul Kumar <rksingh1062005@gmail.com>
2026-06-22 11:42:54 +0530

    Day1 updated

---
+++ 01_CPP/01.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+//Data types & Variables
+    // int age = 10;
+    // char name = 'A';
+    // bool ans = true;
+
+    // //int  -> 4 byte
+    // //long long -> 8 byte
+    // //float -> 4 byte
+    // //double -> 8 byte
+    // //char -> 1 byte
+    // //bool -> 1 byte
+
+    // cout<<age<<endl;
+    // cout<<name<<endl;
+    // cout<<ans<<endl;
+
+
+// conditionals
+
+// int age;
+
+// cout<<"Enter your age: ";
+// cin>>age;
+// cout<<endl;
+
+// int no;
+
+// cout<<"enter your no of tests: ";
+// cin>>no;
+// cout<<endl;
+
+// if(age>18 && no>10){
+//     cout<<"today test will be conducted"<< endl;
+
+// }
+    
+// else{
+//     cout<<"today test will not be conducted"<< endl;
+// }
+
+//switch
+//ternary operator
+
+int a = 10;
+int b = 20;
+(a>b) ? cout<<"a is greater than b" : cout<<"b is greater than a";
+
+
+
+}
+
+
