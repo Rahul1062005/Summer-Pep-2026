9b63f68af53865d7a127a28ccd04b6671f890624
Rahul Kumar <rksingh1062005@gmail.com>
2026-06-23 09:25:22 +0530

    Day2 Updated

--- 01_CPP/Loops.cpp
+++ 01_CPP/Loops.cpp
@@ -3,5 +3,26 @@ using namespace std;
 
 int main()
 {
-    
+    int i = 0;
+
+    for(i=0; i<10; i++)
+    {
+        cout<<i<<endl;
+    }
+
+    while(i<20)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+
+
 }
+
+    
+
+
+
+
+
+
